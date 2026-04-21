ce9e7c286af73c1b083480e7c99050b842c7e832
Angel Ovalles <angelovallesv12@gmail.com>
2026-04-20 21:53:55 -0400

    game logic changed
    -playGame funcion has been deleted
    -disableButtons functions has been added in order to stop the game once the computer or the user reach 5 points
    -added the logic necessary to display the scores, rounds and the icon for the cpu selection

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+let round = 1;
+const userScoreEl = document.getElementById("user-score");
+const cpuScoreEl = document.getElementById("cpu-score");
+const roundEl = document.getElementById("round");
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3) + 1;
   if (randomNumber === 1) {
@@ -9,49 +15,85 @@ function getComputerChoice() {
   }
 }
 
+document
+  .querySelector(".btns-container")
+  .addEventListener("click", function (e) {
+    const btn = e.target.closest("button");
+    if (!btn || btn.disabled) return;
+      let computerChoice = getComputerChoice();
+      playRound(btn.id, computerChoice);
+      renderComputerChoice(computerChoice);
+    
+  });
 
-  document.querySelector(".btns-container").addEventListener("click", function (e) {
-      const btn = e.target.closest("button");
-      if (btn) {
-       console.log(btn.id)
-      }
-    });
-
-function playGame() {
-  let humanScore = 0;
-  let ComputerScore = 0;
-
-  function playRound(humanChoice, computerChoice) {
-    if (humanChoice === computerChoice) {
-      alert("it's a tie");
-    } else if (
-      (humanChoice === "rock" && computerChoice === "scissors") ||
-      (humanChoice === "paper" && computerChoice === "rock") ||
-      (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-      alert(`You won! ${humanChoice} beats ${computerChoice}`);
-      humanScore++;
-    } else {
-      alert(`“You lose! ${computerChoice} beats ${humanChoice}`);
-      ComputerScore++;
-    }
-  }
-  for (let i = 0; i < 5; i++) {
-    const humanSelection = getHumanChoice();
-    const computerSelection = getComputerChoice();
-    playRound(humanSelection, computerSelection);
-  }
-  if (humanScore > ComputerScore) {
-    alert(
-      `You won! Your score: ${humanScore} vs Computer score ${ComputerScore}`,
-    );
-  } else if (humanScore === ComputerScore) {
-    alert("it's a tie");
-  } else {
-    alert(
-      `You lost Your score: ${humanScore} vs Computer score ${ComputerScore}`,
-    );
+function renderComputerChoice(computerChoice) {
+  let icon = document.querySelector(".cpu-selection-container");
+  if (computerChoice === "rock") {
+    icon.innerHTML = `
+   <img id="selection-icon" src="icons/rock.png">
+      
+      `;
+  } else if (computerChoice === "paper") {
+    icon.innerHTML = `
+   <img id="selection-icon" src="icons/paper.png">
+      
+      `;
+  } else if (computerChoice === "scissors") {
+    icon.innerHTML = `
+   <img id="selection-icon" src="icons/scissors.png">
+      
+      `;
   }
 }
 
-// playGame();
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    alert("it's a tie");
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    alert(`You won! ${humanChoice} beats ${computerChoice}`);
+    humanScore++;
+  } else {
+    alert(`“You lose! ${computerChoice} beats ${humanChoice}`);
+    computerScore++;
+  }
+  round++;
+  updateScores();
+}
+function updateScores() {
+  userScoreEl.textContent = humanScore;
+  cpuScoreEl.textContent = computerScore;
+  roundEl.textContent = round;
+
+  if (humanScore === 5) {
+    alert(
+      `You won! Your score: ${humanScore} vs Computer score ${computerScore}. Press f5 to play again`,
+    );
+    disableButtons();
+  } else if (computerScore === 5) {
+    alert(`You lost Your score: ${humanScore} vs Computer score ${computerScore}. Press f5 to play again`)
+      disableButtons();
+  }
+}
+
+function disableButtons() {
+const rockBtn = document.getElementById('rock')
+const paperBtn = document.getElementById("paper");
+const scissorsBtn = document.getElementById("scissors");
+
+rockBtn.disabled = true;
+paperBtn.disabled = true;
+scissorsBtn.disabled = true;
+
+rockBtn.style.opacity = "0.5"; 
+paperBtn.style.opacity = "0.5"; 
+scissorsBtn.style.opacity = "0.5"; 
+
+rockBtn.style.cursor = "not-allowed";
+paperBtn.style.cursor = "not-allowed";
+scissorsBtn.style.cursor = "not-allowed";
+
+}
